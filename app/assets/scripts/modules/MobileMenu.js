fb8765e22af0e8022939594e30309a297bb5ce1a
Mark Ye <mark.fengye@gmail.com>
2020-06-14 00:04:29 -0700

    Completed header section including mobile menu.

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,20 @@
+class MobileMenu {
+  constructor() {
+    this.menuIcon = document.querySelector(".site-header__menu-icon");
+    this.menuContent = document.querySelector(".site-header__menu-content");
+    this.siteHeader = document.querySelector(".site-header");
+    this.events();
+  }
+
+  events() {
+    this.menuIcon.addEventListener("click", () => this.toggleMenu());
+  }
+
+  toggleMenu() {
+    this.menuContent.classList.toggle("site-header__menu-content--visible");
+    this.siteHeader.classList.toggle("site-header--expanded");
+    this.menuIcon.classList.toggle("site-header__menu-icon--x");
+  }
+}
+
+export default MobileMenu;

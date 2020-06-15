2fda57c1ba546230f277f08bbfdad69f3f50fe0a
Mark Ye <mark.fengye@gmail.com>
2020-06-15 15:21:33 -0700

    learning cloud function.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,28 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Super Area."
-  });
+  const responseHTML = `
+  <h3>Welcome to the Secret Area.</h3>
+    <p>For registered customer only.</p>
+  `;
+
+  let body;
+
+  // event.body is the raw data json string sent along via POST/GET
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+  // assuming predefined key is "password", and expecting value of "theCorrectPassword"
+  if (body.password == "theCorrectPassword") {
+    callback(null, {
+      // the response
+      statusCode: 200,
+      body: responseHTML
+    });
+  } else {
+    callback(null, {
+      // forbiden
+      statusCode: 401
+    });
+  }
 };

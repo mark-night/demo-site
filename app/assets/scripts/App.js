83481181d7acf4c60e85738fe350af7143211658
Mark Ye <mark.fengye@gmail.com>
2020-06-14 22:17:03 -0700

    Modal functions added.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,9 @@ import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
+
+new Modal();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 0.8);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 0.7);
